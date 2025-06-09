18a297d90547fc4e8526999d1f8ff43e66952045
Nico Aramy <nico1999rzkny@gmail.com>
2025-06-09 23:59:35 +0700

    change background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,15 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1 style={{
+        fontSize: "2.5rem",
+        fontWeight: "bold",
+        color: "#06258a",
+        marginBottom: 16,
+        textAlign: "center"
+      }}>
+        PPDB Tahun 2026-2027
+      </h1>
       <h1 className={styles.title} style={{ color: "black" }}>Selamat Datang di TK IT Cendekia</h1>
       <h2 className={styles.title}></h2>
       <div style={{ marginTop: 32, display: 'flex', gap: 16 }}>
